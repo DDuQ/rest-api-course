2d395d9ab77c22351fdabe49fa52fcad2c210492
DDuQ <61994091+DDuQ@users.noreply.github.com>
2025-03-13 17:14:44 -0500

    Add cancellation token

---
+++ Movies.API/Mapping/ValidationMappingMiddleware.cs
@@ -0,0 +1,36 @@
+using FluentValidation;
+using Movies.Contracts.Responses;
+
+namespace Movies.API.Mapping;
+
+public class ValidationMappingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ValidationMappingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ValidationException exception)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            var validationFailureResponse = new ValidationFailureResponse
+            {
+                Errors = exception.Errors.Select(error => new ValidationResponse()
+                {
+                    PropertyName = error.PropertyName,
+                    Message = error.ErrorMessage,
+                }),
+            };
+            
+            await context.Response.WriteAsJsonAsync(validationFailureResponse);
+        }
+    }
+}
